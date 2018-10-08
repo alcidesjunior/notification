710d8d0d2c2aa98dd41593c4e7fc77350c4b9adf
Alcides Junior <alcidesjunior@10-45-48-63.0200.ifce.br.iosda.org>
2018-10-08 09:16:28 -0300

    Initial Commit

---
+++ TryNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryNotification
+//
+//  Created by Alcides Junior on 08/10/18.
+//  Copyright © 2018 Alcides Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TryNotificationTests/TryNotificationTests.swift
@@ -0,0 +1,34 @@
+//
+//  TryNotificationTests.swift
+//  TryNotificationTests
+//
+//  Created by Alcides Junior on 08/10/18.
+//  Copyright © 2018 Alcides Junior. All rights reserved.
+//
+
+import XCTest
+@testable import TryNotification
+
+class TryNotificationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
